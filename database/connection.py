acd4ab005de22c71920b46cad4d51f33959ee416
shreyvirmani <shreyvirmani1905@gmail.com>
2026-07-22 18:16:44 +0530

    Fix production database configuration

--- database/connection.py
+++ database/connection.py
@@ -7,7 +7,7 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 
 if not DATABASE_URL:
-    raise RuntimeError(
+    raise ValueError(
         "DATABASE_URL environment variable is missing"
     )
 
@@ -16,6 +16,9 @@ engine = create_engine(
     DATABASE_URL,
     pool_pre_ping=True,
     pool_recycle=300,
+    connect_args={
+        "sslmode": "require"
+    }
 )
 
 
